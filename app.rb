f92e88836d424d209bce07062948326e045f8222
ILyass-Lr <lirmaqui.ilyass@gmail.com>
2025-04-28 00:30:00 +0100

    Added CSS and Views.

--- app.rb
+++ app.rb
@@ -4,5 +4,13 @@ require 'sinatra/reloader' if development?
 set :port, 4517
 
 get '/' do
-	'hello, world!'
+	erb :index
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/palindrome' do
+	erb :palindrome
 end
